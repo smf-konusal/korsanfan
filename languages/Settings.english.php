63fbe2d71189f72b326bbdac27b5a661e8317f6d
CeeMoo <qdale.123@gmail.com>
2021-09-07 21:41:09 +0300

    up

--- languages/Settings.english.php
+++ languages/Settings.english.php
@@ -5,6 +5,6 @@ global $settings;
 
 // argument(s): images_url as saved in settings
 $txt['theme_thumbnail_href'] = '%1$s/thumbnail.png';
-$txt['theme_description'] = '<strong>Bese Theme By <a href="https://smf.konusal.com/" target="_blank">Cee</a></strong>';
+$txt['theme_description'] = '<strong>KorsanFAN Theme By <a href="https://smf.konusal.com/" target="_blank">Cee</a></strong>';
 
 ?>
